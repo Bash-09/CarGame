0f903e1e7248221adc6659966ddb51b311fbf2f4
Ethan Miegel <ethan.miegel@gmail.com>
2022-11-16 21:49:49 +1100

    Created player and a movement script

--- Godot/scenes/Player.gd
+++ Godot/scenes/Player.gd
@@ -1,17 +1,55 @@
-extends Area2D
+extends Sprite
 
-#Hi ethan!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! :)))))))))))))))))) 
+var max_velocity = 500
+var acceleration = 300
+var deceleration_rate = 0.95
+var turn_rate = 3
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var velocity: Vector2 = Vector2.ZERO
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	velocity.x = 400
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	
+	move(delta)
+	
+	
+	
+	
+func move(delta):
+	# Check input
+	var drifting: bool = Input.is_mouse_button_pressed(2)
+	var accelerating: bool = Input.is_mouse_button_pressed(1)
+	
+	# Find some stuff about where we want to drive towards
+	var mouse_pos = get_viewport().get_mouse_position()
+	
+	# Clamp target position to be in front of the car to avoid driving backwards
+	if mouse_pos.x < position.x:
+		mouse_pos.x = position.x
+	
+	var target_vector: Vector2 = (mouse_pos - position).normalized()
+	var facing_vector: Vector2 = Vector2.RIGHT.rotated(rotation)
+	
+	var turn: float = facing_vector.angle_to(target_vector)
+	
+	# Accelerate
+	if accelerating:
+		if velocity.length_squared() < 1:
+			velocity += Vector2.RIGHT.rotated(rotation) * acceleration * delta
+		else:
+			velocity += velocity.normalized() * acceleration * delta
+		
+		velocity = velocity.rotated(turn * delta * turn_rate)
+		if velocity.length_squared() > max_velocity * max_velocity:
+			velocity = velocity.normalized() * max_velocity
+	else:
+		velocity -= velocity * deceleration_rate * delta
+	
+	# Move
+	position += velocity * delta;
+	rotation = velocity.angle()
+	
+	# Just simulates a scrolling screen
+	position.x -= 450 * delta
