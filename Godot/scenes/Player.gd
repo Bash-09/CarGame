438beac00083541c32776f34bee513b1e033cb41
Ethan Miegel <ethan.miegel@gmail.com>
2022-11-18 20:45:09 +1100

    Epic drifting

--- Godot/scenes/Player.gd
+++ Godot/scenes/Player.gd
@@ -2,29 +2,30 @@ extends Area2D
 
 signal hit
 
-var max_velocity = 500
-var acceleration = 300
+var max_velocity = 450
+var acceleration = 250
 var deceleration_rate = 0.95
-var turn_rate = 3
+var turn_rate = 4
 #Exported some variables so we can easily tweak if needed
 export var movespeed = 300;
 
 var velocity: Vector2 = Vector2.ZERO
+var was_drifting: bool = false;
 
 func _ready():
-	velocity.x = 400
+	velocity.x = 800
 
 func _process(delta):
-	
 	move(delta)
-	
-	
 	
 	
 func move(delta):
 	# Check input
 	var drifting: bool = Input.is_mouse_button_pressed(2)
 	var accelerating: bool = Input.is_mouse_button_pressed(1)
+	
+	if drifting:
+		was_drifting = true
 	
 	# Find some stuff about where we want to drive towards
 	var mouse_pos = get_viewport().get_mouse_position()
@@ -38,22 +39,41 @@ func move(delta):
 	
 	var turn: float = facing_vector.angle_to(target_vector)
 	
-	# Accelerate
 	if accelerating:
+		# Accelerate
 		if velocity.length_squared() < 1:
 			velocity += Vector2.RIGHT.rotated(rotation) * acceleration * delta
 		else:
-			velocity += velocity.normalized() * acceleration * delta
+			velocity += facing_vector * acceleration * delta
 		
-		velocity = velocity.rotated(turn * delta * turn_rate)
-		if velocity.length_squared() > max_velocity * max_velocity:
-			velocity = velocity.normalized() * max_velocity
+		# Turn the vehicle a bit
+		rotation += turn * delta * turn_rate
+		
+		if drifting:
+			# Reduce how much our velocity turns with us and add some acceleration in the direction we are actually facing
+			velocity = velocity.rotated(turn * delta * turn_rate / 2)
+			velocity += facing_vector * acceleration * delta
+			velocity -= velocity.normalized() * facing_vector * acceleration * delta
+		
+		else:
+			# Turn our vehicle's velocity to match the vehicle itself turning
+			velocity = velocity.rotated(turn * delta * turn_rate)
+			# Reduce velocity in directions that aren't where we are facing
+			var normal = Vector2.RIGHT.rotated(rotation + PI/2)
+			normal *= velocity.dot(normal)
+			velocity -= normal * delta * 5
+			
+		# Bring velocity back down to max_velocity
+		if velocity.length_squared() > (max_velocity * max_velocity):
+			velocity -= velocity.normalized() * delta * max_velocity
+	
 	else:
+		# Decelerate
 		velocity -= velocity * deceleration_rate * delta
+	
 	
 	# Move
 	position += velocity * delta;
-	rotation = velocity.angle()
 	
 	# Just simulates a scrolling screen
 	position.x -= movespeed * delta
