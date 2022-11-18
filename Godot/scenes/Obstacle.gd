50078ed4fff13a00db746fbc3bed5d236f172c8c
Ethan Miegel <ethan.miegel@gmail.com>
2022-11-18 22:23:29 +1100

    Refactored Obstacle and fixed collision issue

--- Godot/scenes/Obstacle.gd
+++ Godot/scenes/Obstacle.gd
@@ -1,38 +1,20 @@
 extends Area2D
 
-var obs_tex1 = preload("res://assets/graphics/obs1.png")
-var obs_tex2 = preload("res://assets/graphics/obs2.png")
-var obs_tex3 = preload("res://assets/graphics/obs3.png")
-var obs_tex4 = preload("res://assets/graphics/obs4.png")
-
 export var movespeed = 330;
 
-onready var obsSprite = get_node("Sprite")
-
+const images = [
+	preload("res://assets/graphics/obs1.png"),
+	preload("res://assets/graphics/obs2.png"),
+	preload("res://assets/graphics/obs3.png"),
+	preload("res://assets/graphics/obs4.png")
+]
 
 func _ready():
-	#This segment handles applying a random texture to the obstacle
-	#I will need to add a bit 
-	randomize()
-	var myRand = randi()%4+1
-	if myRand == 1:
-		obsSprite.set_texture(obs_tex1)
-	if myRand == 2:
-		obsSprite.set_texture(obs_tex2)
-	if myRand == 3:
-		obsSprite.set_texture(obs_tex3)
-	if myRand == 4:
-		obsSprite.set_texture(obs_tex4)
-	#I have no idea why this does not work properly, need to fix later, maybe hardcode idk
-	#Get the size of the sprite
-	var obsWitdth = obsSprite.texture.get_width()
-	var obsHeight = obsSprite.texture.get_height()
-	#Update the obstacle's collision shape to be the size of the sprite
-	$CollisionShape2D.shape.extents.x = obsWitdth / 2
-	$CollisionShape2D.shape.extents.y = obsHeight / 2
+	var chosenTexture = images[randi()%images.size()]
 	
+	$Sprite.set_texture(chosenTexture)
+	$CollisionShape2D.shape.set_extents(chosenTexture.get_size() / 2)
 	
-
 
 func _process(delta):
 	#Yes very big brain ethan, multiplying by delta so that the object does not instantly
@@ -43,7 +25,9 @@ func _process(delta):
 	if position.x < -200:
 		queue_free()
 	
+
 #Controls what happens to the obstacle when being hit by the car
 func destroy():
 	hide()
 	queue_free()
+	
